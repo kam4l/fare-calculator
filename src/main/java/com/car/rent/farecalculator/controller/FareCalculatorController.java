0aea6c0a39a808b1824ec843b2b6d076bd765fc8
kam4l <mkamalcool90@gmail.com>
2019-06-13 19:08:20 +0100

    Added swagger implementation

--- src/main/java/com/car/rent/farecalculator/controller/FareCalculatorController.java
+++ src/main/java/com/car/rent/farecalculator/controller/FareCalculatorController.java
@@ -3,6 +3,9 @@ package com.car.rent.farecalculator.controller;
 import com.car.rent.farecalculator.beans.CarRentalEnquiryRequest;
 import com.car.rent.farecalculator.beans.CarRentalExpense;
 import com.car.rent.farecalculator.service.CalculateFareService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +26,11 @@ public class FareCalculatorController {
     }
 
     @PostMapping("/calculateFare")
+    @ApiOperation(value = "calculate the fare for the given trip params", response = CarRentalExpense.class)
+    @ApiResponses(value = {
+            @ApiResponse(code= 200, message = "successfully calculated fare"),
+            @ApiResponse(code= 400, message = "invalid input parameter(s) supplied")
+    })
     public CarRentalExpense calculateFare(@Valid @RequestBody CarRentalEnquiryRequest rentalEnquiryRequest) {
         validateRequest(rentalEnquiryRequest);
         return calculateFareService.calculateCarRentalExpense(rentalEnquiryRequest);
